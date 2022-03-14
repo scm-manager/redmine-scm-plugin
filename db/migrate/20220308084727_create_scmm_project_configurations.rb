d772d71808bc7722dc97ffbc800b7777bf3120b9
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-03-14 12:05:42 +0100

    Fix references on mysql by specifying id type integer

--- db/migrate/20220308084727_create_scmm_project_configurations.rb
+++ db/migrate/20220308084727_create_scmm_project_configurations.rb
@@ -1,7 +1,7 @@
 class CreateScmmProjectConfigurations < ActiveRecord::Migration[5.2]
   def change
     create_table(:scmm_project_configurations, primary_key: [:project_id]) do |t|
-      t.references :project, foreign_key: { on_delete: :cascade }
+      t.references :project, type: :integer, foreign_key: { on_delete: :cascade }
       t.string :scm_url
       t.string :bug_tracker, default: "Bug"
     end
--- db/migrate/20220310075034_create_scmm_issue_connected_branches.rb
+++ db/migrate/20220310075034_create_scmm_issue_connected_branches.rb
@@ -1,7 +1,7 @@
 class CreateScmmIssueConnectedBranches < ActiveRecord::Migration[5.2]
   def change
     create_table :scmm_issue_connected_branches do |t|
-      t.references :issue, foreign_key: { on_delete: :cascade }
+      t.references :issue, type: :integer, foreign_key: { on_delete: :cascade }
       t.string :branch_name
     end
   end
