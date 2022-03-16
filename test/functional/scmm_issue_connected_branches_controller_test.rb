67ef8a387568411a36c29fe4360a9065f9f0384a
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-03-16 11:55:54 +0100

    add working example test

--- test/functional/scmm_issue_connected_branches_controller_test.rb
+++ test/functional/scmm_issue_connected_branches_controller_test.rb
@@ -1,8 +1,26 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ScmmIssueConnectedBranchesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  fixtures :projects,
+           :issues,
+           :users,
+           :roles,
+           :members,
+           :member_roles
+
+  def setup
+    User.current = nil
+  end
+
+  def test_new
+    @request.session[:user_id] = 2
+    Role.find(1).add_permission! :scmm_connect_branches
+    Project.find(1).enabled_module_names = [:scm_manager]
+
+    get :new, params: { :issue_id => 1, :project_id => 1 }
+
+    assert_response :success
+    assert_template 'new'
   end
 end
