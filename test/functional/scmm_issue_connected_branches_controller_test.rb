290db5c86114ded9bdc9b032a0ce729f3c9865c6
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2022-03-16 13:40:00 +0100

    Add tests

--- test/functional/scmm_issue_connected_branches_controller_test.rb
+++ test/functional/scmm_issue_connected_branches_controller_test.rb
@@ -10,17 +10,54 @@ class ScmmIssueConnectedBranchesControllerTest < ActionController::TestCase
            :member_roles
 
   def setup
-    User.current = nil
-  end
-
-  def test_new
+    User.current = User.find(1)
     @request.session[:user_id] = 2
     Role.find(1).add_permission! :scmm_connect_branches
     Project.find(1).enabled_module_names = [:scm_manager]
+  end
 
+  def test_new
     get :new, params: { :issue_id => 1, :project_id => 1 }
 
     assert_response :success
     assert_template 'new'
+    assert_select '#scmm_issue_connected_branch_branch_name' do
+      assert_select "[value=?]", "feature/1_cannot_print_recipes"
+    end
+  end
+
+  def test_create
+    post(
+      :create,
+      :params => {
+        :issue_id => 1,
+        :scmm_issue_connected_branch => {
+          :id => 1,
+          :issue_id => 1,
+          :branch_name => 'feature/good_one'
+        }
+      }
+    )
+    assert_redirected_to "http://test.host/issues/1"
+    relation = ScmmIssueConnectedBranch.order('id DESC').first
+    assert_equal 1, relation.issue.id
+    assert_equal 'feature/good_one', relation.branch_name
+  end
+
+  def test_create_with_invalid_branch
+    post(
+      :create,
+      :params => {
+        :issue_id => 1,
+        :scmm_issue_connected_branch => {
+          :id => 1,
+          :issue_id => 1,
+          :branch_name => ''
+        }
+      }
+    )
+    assert_response :success
+    relation = ScmmIssueConnectedBranch.first
+    assert_nil relation
   end
 end
