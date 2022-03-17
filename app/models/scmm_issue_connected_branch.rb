f0ffb1b99db161d317218ad6c34c3d02f953e32f
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2022-03-17 09:54:19 +0100

    Add regex validation for branch name

--- app/models/scmm_issue_connected_branch.rb
+++ app/models/scmm_issue_connected_branch.rb
@@ -1,4 +1,6 @@
 class ScmmIssueConnectedBranch < ActiveRecord::Base
   belongs_to :issue
-  validates :branch_name, length: { in: 2..255 }
+  validates :branch_name,
+            length: { in: 2..255 },
+            format: { with: /\A[\w\-,;\]{}@&+=$#`|<>]([\w\-,;\]{}@&+=$#`|<>\/.]*[\w\-,;\]{}@&+=$#`|<>])?\z/ }
 end
